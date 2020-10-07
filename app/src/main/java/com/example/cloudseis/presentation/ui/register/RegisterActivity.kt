42c4679df35e7c99a2d3c5213c1e7604e132f99d
Paramorel <sendzyak2017@yandex.ru>
2020-10-07 17:12:57 +0700

    main activity ready

--- app/src/main/java/com/example/cloudseis/presentation/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/cloudseis/presentation/ui/register/RegisterActivity.kt
@@ -1,7 +1,6 @@
 package com.example.cloudseis.presentation.ui.register
 
 import androidx.appcompat.app.AppCompatActivity
-import com.example.cloudseis.presentation.ui.ViewContract
 
 class RegisterActivity : AppCompatActivity(), RegisterView{
 
